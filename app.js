1269d95bf32c6062a705a3362c0a1d3801d7b553
AliDemircix <alidemircix@gmail.com>
2021-09-08 18:49:12 +0200

    Game Number added and released v.0.1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const game = () => {
     let scoreP=0;
     let scoreC=0;
+    let gameNumber=1;
 
     const startGame = () => {
         const playBtn = document.querySelector('.play-btn');
@@ -11,6 +12,14 @@ const game = () => {
            introScreen.classList.add('fadeOut');
            matchScreen.classList.add('fadeIn');
            scoresScreen.classList.add('fadeIn');
+           const gNumbers = document.querySelectorAll('input[name="choice"]');
+            
+            for (const gNumber of gNumbers) {
+                if (gNumber.checked) {
+                    gameNumber = gNumber.value;
+                    break;
+                }
+            }
        })
     }
 
@@ -33,7 +42,13 @@ const game = () => {
             const computerSelection = computerOption[computerHandNumber];
             console.log(computerSelection);
             computerHand.src=`./img/${computerSelection}.png`;
+           
             compareHands(playerSelection, computerSelection);
+            // Game number decrease
+            
+           
+      
+       
             
         })
     })
@@ -44,6 +59,7 @@ const game = () => {
         const computerScore = document.querySelector('.computer-score p');
         playerScore.textContent =scoreP;
         computerScore.textContent =scoreC;
+
         if(parseInt(playerScore.textContent)>parseInt(computerScore.textContent)){
             playerScore.style.color="green";
             computerScore.style.color="red";
@@ -59,84 +75,129 @@ const game = () => {
         }
 
     };
-    const compareHands = (playerSelection, computerSelection) => {
-        const winner = document.querySelector('.winner');
-        if (playerSelection==="stone"){
-            if(computerSelection==="paper"){
-                winner.innerText= 'Computer Win';
-                winner.style.color="red";
-                console.log('Computer Win')
-                scoreC++;
-                updateScore();
-                return;
-            }
-            else if(computerSelection==="stone") {
-                console.log('Tied Game')
-                winner.innerText= 'Tied Game';
-                winner.style.color="blue";
-                return;
-            }
-            else {
-                winner.innerText= 'Player Win';
-                winner.style.color="white";
-                scoreP++;
-                updateScore();
-                return;
-            }
-        }
-        else if(playerSelection==="paper"){
-            if(computerSelection==="scissors"){
-                winner.innerText= 'Computer Win';
-                winner.style.color="red";
-                scoreC++;
-                updateScore();
-                console.log('Computer Win')
-                return;
-            }
-            else if(computerSelection==="paper"){
-                winner.innerText= 'Tied Game';
-                winner.style.color="blue";
-                console.log('Tied Game')
-                return;
-            }
-            else {
-                console.log('Player Win');
-                winner.innerText= 'Player Win';
-                winner.style.color="white";
-                scoreP++;
-                updateScore();
-                return;
-            }
-        }
-        else {
-            if(computerSelection==="stone"){
-                winner.innerText= 'Computer Win';
-                winner.style.color="red";
-                scoreC++;
-                updateScore();
-                console.log('Computer Win');
-                return;
-            }
-           else if(computerSelection==="scissors"){
-                winner.innerText= 'Tied Game';
-                winner.style.color="blue";
-                console.log('Tied Game');
-                return;
-            }
-            else {
-                winner.innerText= 'Player Win';
-                winner.style.color="white";
-                scoreP++;
-                updateScore();
-                console.log('Player Win');
-                return;
-            }
-        }
-    
 
+    const compareGame = ()=> {
+        const introScreen = document.querySelector('.intro h1');
+      
+      
+        if(scoreP>scoreC) {
+            introScreen.innerText= 'GAME OVER YOU WIN '; 
+            introScreen.style.color="green";
+        }
+        else if(scoreP<scoreC){
+            introScreen.innerText= 'GAME OVER YOU LOST ';
+            introScreen.style.color="red";
+        }
+        else if(scoreP=scoreC){
+            introScreen.innerText= 'GAME OVER THERE IS NO WINNER ';
+        }
+       
+      return;      
+    
     }
 
+    const compareHands = (playerSelection, computerSelection) => {
+        const winner = document.querySelector('.winner');
+
+        gameNumber--;
+        console.log(gameNumber);
+        if(gameNumber>=0){
+            if (playerSelection==="stone"){
+                if(computerSelection==="paper"){
+                    winner.innerText= 'Computer Win';
+                    winner.style.color="red";
+                    console.log('Computer Win')
+                    scoreC++;
+                    updateScore();
+                    return;
+                }
+                else if(computerSelection==="stone") {
+                    console.log('Tied Game')
+                    winner.innerText= 'Tied Game';
+                    winner.style.color="blue";
+                    updateScore();
+                    return;
+                }
+                else {
+                    winner.innerText= 'Player Win';
+                    winner.style.color="white";
+                    scoreP++;
+                    updateScore();
+                    return;
+                }
+            }
+            else if(playerSelection==="paper"){
+                if(computerSelection==="scissors"){
+                    winner.innerText= 'Computer Win';
+                    winner.style.color="red";
+                    scoreC++;
+                    updateScore();
+                    console.log('Computer Win')
+                    return;
+                }
+                else if(computerSelection==="paper"){
+                    winner.innerText= 'Tied Game';
+                    winner.style.color="blue";
+                    console.log('Tied Game')
+                    updateScore();
+                    return;
+                }
+                else {
+                    console.log('Player Win');
+                    winner.innerText= 'Player Win';
+                    winner.style.color="white";
+                    scoreP++;
+                    updateScore();
+                    return;
+                }
+            }
+            else {
+                if(computerSelection==="stone"){
+                    winner.innerText= 'Computer Win';
+                    winner.style.color="red";
+                    scoreC++;
+                    updateScore();
+                    console.log('Computer Win');
+                    return;
+                }
+               else if(computerSelection==="scissors"){
+                    winner.innerText= 'Tied Game';
+                    winner.style.color="blue";
+                    console.log('Tied Game');
+                    updateScore();
+                    return;
+                }
+                else {
+                    winner.innerText= 'Player Win';
+                    winner.style.color="white";
+                    scoreP++;
+                    updateScore();
+                    console.log('Player Win');
+                    return;
+                }
+            }
+        
     
+        }
+        else {
+            compareGame();
+            const introScreen = document.querySelector('.intro');
+            const matchScreen = document.querySelector('.match');
+            const scoresScreen = document.querySelector('.scores');
+            introScreen.classList.remove('fadeOut');
+            matchScreen.classList.remove('fadeIn');
+            scoresScreen.classList.add('fadeIn');
+            const playBtn = document.querySelector('.play-btn');
+            playBtn.addEventListener('click', function(){
+                window.location.reload();
+            })
+        }
+
+        }
+
+        
+
+
 
     startGame();
    playMatch();
